39196a67ba1a3866e2dbd74f93f62d13a1a0747f
WayupKG <adikgk232323@gmail.com>
2022-05-30 23:30:32 +0600

    Add include service

--- bavaria/urls.py
+++ bavaria/urls.py
@@ -7,6 +7,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('service/', include('service.urls')),
 ]
 
 if settings.DEBUG:
