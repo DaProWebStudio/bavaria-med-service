c468ac15e0e8594d2eccdef44ed5ca1b080c4d5d
WayupKG <adikgk232323@gmail.com>
2022-05-30 23:28:46 +0600

    Created GreetingView

--- core/views.py
+++ core/views.py
@@ -4,5 +4,8 @@ from django.views.generic import View, TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
-
-
+    
+    
+class GreetingView(TemplateView):
+    template_name = 'greeting.html'
+    
