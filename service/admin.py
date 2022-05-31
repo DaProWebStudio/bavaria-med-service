2564eecab5d2db6ce0aeb53b309b2276d1dadfc8
WayupKG <adikgk232323@gmail.com>
2022-05-31 22:05:16 +0600

    Register admin to model Service

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Service
 
-# Register your models here.
+
+@admin.register(Service)
+class ServiceNews(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
