d5d3006ac6119e20b5c9536f4e5dd024a9280d1b
WayupKG <Adikgk232323@gmail.com>
2022-06-10 23:32:06 +0600

    Rename curren => current, add corrent_year

--- core/services/context_processors.py
+++ core/services/context_processors.py
@@ -1,10 +1,13 @@
 from typing import Any
 
+from django.utils import timezone
+
 
 def getting_info(request: Any) -> dict[str, Any]:
     domen = 'https://btk.kg'
-    curren_path = request.get_full_path()
-    curren_url = f'{domen}{curren_path}'
+    current_path = request.get_full_path()
+    current_url = f'{domen}{current_path}'
+    corrent_year = timezone.now().strftime('%Y')
     tel_number = {'format': '+996 (707) 36-67-41', 'href': '+996707366741'}
     mobile_number = {'format': '+996 (707) 36-67-41', 'href': '+996707366741'}
     email = 'bavariamed@gmail.com'
