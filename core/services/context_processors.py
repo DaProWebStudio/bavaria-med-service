68b58612b85bb46ebac3d71ef4a7624a88421fb7
WayupKG <Adikgk232323@gmail.com>
2022-06-12 01:33:19 +0600

    Update Phone number format, update admin

--- core/services/context_processors.py
+++ core/services/context_processors.py
@@ -6,7 +6,7 @@ def getting_info(request):
     current_path = request.get_full_path()
     current_url = f'{domen}{current_path}'
     corrent_year = timezone.now().strftime('%Y')
-    tel_number = {'format': '+996 (707) 36-67-41', 'href': '+996707366741'}
-    mobile_number = {'format': '+996 (707) 36-67-41', 'href': '+996707366741'}
+    tel_number = {'format': '+996 (707) 36-67-44', 'href': '+996707366744'}
+    mobile_number = {'format': '+996 (707) 36-67-45', 'href': '+996707366745'}
     email = 'bavariamed@gmail.com'
     return locals()
