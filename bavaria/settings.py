7a98f98ba97c394e64dd6f7cd772cb2f0e1f9cba
WayupKG <Adikgk232323@gmail.com>
2022-06-19 09:40:21 +0600

    fix: environs

--- bavaria/settings.py
+++ bavaria/settings.py
@@ -3,10 +3,11 @@ import environs
 from pathlib import Path
 
 env = environs.Env()
-env.read_env('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
